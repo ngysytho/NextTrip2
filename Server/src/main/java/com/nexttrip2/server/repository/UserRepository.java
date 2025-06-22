31959bf78c6172cb855004d3231c6aea1b2cd0fe
Nguyen Sy Tho <ngysytho@SyThos-MacBook-Pro.local>
2025-06-22 11:56:23 +0700

    WIP: cập nhật local trước khi rebase

--- Server/src/main/java/com/nexttrip2/server/repository/UserRepository.java
+++ Server/src/main/java/com/nexttrip2/server/repository/UserRepository.java
@@ -5,18 +5,32 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.nexttrip2.server.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
-    @Query("{ 'email_user': ?0 }")
+    @Query("{ 'email_user': ?0, 'isActive_user': true }")
+    Optional<User> findActiveUserByEmail(String email_user);
+
+    @Query("{ 'username_user': ?0, 'isActive_user': true }")
+    Optional<User> findActiveUserByUsername(String username_user);
+
+    @Query(value = "{ 'email_user': ?0, 'isActive_user': true }", exists = true)
+    Boolean existsActiveByEmail(String email_user);
+
+    @Query(value = "{ 'username_user': ?0, 'isActive_user': true }", exists = true)
+    Boolean existsActiveByUsername(String username_user);
+
+    @Query(value = "{ 'email_user': ?0 }", exists = true)
     Boolean existsByEmail_user(String email_user);
 
-    @Query("{ 'email_user': ?0 }")
-    User findByEmail_user(String email_user); // ✅ Đã sửa tên đúng
-
-    @Query("{ 'username_user': ?0 }")
+    @Query(value = "{ 'username_user': ?0 }", exists = true)
     Boolean existsByUsername_user(String username_user);
 
+    @Query("{ 'email_user': ?0 }")
+    Optional<User> findByEmail_user(String email_user);
+
     @Query("{ 'verifyToken_user': ?0 }")
-    User findByVerifyToken_user(String verifyToken_user);
+    Optional<User> findByVerifyToken_user(String verifyToken_user);
 }
